193300e7bca37af0fd53e2c474d72a85915a70ee
Sean Hull <sean.hull@vice.com>
2018-01-14 12:14:02 -0500

    hackerrank day 1 challenge

---
+++ hrd1.py
@@ -0,0 +1,23 @@
+
+import sys
+
+#lines = sys.stdin.readlines
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+anint = int(raw_input())
+afloat = float(raw_input())
+astring = raw_input()
+
+anint = i + anint
+afloat = d + afloat
+astring = s + astring
+
+print ("INT: %d" % anint)
+print ("FLOAT: %.1f" % afloat)
+print ("STRING: %s" % astring)
+
+
+       
