3837da43dc48e85a3f44dcbb0324a0475f7ec497
Sean Hull <hullsean@gmail.com>
2018-02-08 13:16:09 -0500

    qualify.com code challege - interesting but timer ran out.  grrr

---
+++ qualify.py
@@ -0,0 +1,13 @@
+
+# Complete this function to return either
+# "Hello, [name]!" or "Hello there!"
+# based on the input
+
+def say_hello(name):
+  # you can print to STDOUT to debug if you need to:
+  print("Hello %s!" % name)
+  
+  # but to complete the challenge you need to return a value
+  return ("Hello %s!" % name) # TODO: return correct value
+  
+  
